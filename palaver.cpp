009d65533f6c749e2d629566a2fe26cd461d536f
Kyle Fuller <inbox@kylefuller.co.uk>
2013-04-24 21:28:53 +0100

    Send messages even if there are online clients

--- palaver.cpp
+++ palaver.cpp
@@ -990,7 +990,7 @@ public:
 #pragma mark -
 
 	void ParseMessage(CNick& Nick, CString& sMessage, CChan *pChannel = NULL) {
-		if (m_pNetwork->IsUserOnline() == false) {
+		if (true || m_pNetwork->IsUserOnline() == false) {
 #if defined VERSION_MAJOR && defined VERSION_MINOR && VERSION_MAJOR >= 1 && VERSION_MINOR >= 2
 			CString sCleanMessage = sMessage.StripControls_n();
 #else
