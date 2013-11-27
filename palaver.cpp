37e531b8414e99640a5162f50e44b84db2758c43
Thasan <thasan@koodaa.net>
2013-11-27 07:52:54 +0200

    Removing color codes

--- palaver.cpp
+++ palaver.cpp
@@ -828,6 +828,9 @@ public:
 						device.HasMentionNick(Nick.GetNick()) ||
 						device.IncludesMentionKeyword(sMessage, m_pNetwork->GetIRCNick().GetNick()));
 
+					sMessage.Replace("\x03","");
+					sMessage.Replace("\x0F","");
+
 					if (bMention && (
 							(pChannel && device.HasIgnoreChannel(pChannel->GetName())) ||
 							device.HasIgnoreNick(Nick.GetNick()) ||
