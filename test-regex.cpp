7ba071459ac45e0d2fbb1541493be2c3affbcac8
Kyle Fuller <kyle@fuller.li>
2017-01-13 03:02:28 +0000

    test: show raw exception

--- test-regex.cpp
+++ test-regex.cpp
@@ -7,13 +7,8 @@ int main(int argc, const char *argv[]) {
     std::string message = "Hello nickname.";
     std::string matcher = "\\bnickname\\b";
 
-    try {
-        std::regex expression = std::regex(matcher, std::regex_constants::ECMAScript | std::regex_constants::icase);
-        std::regex_search(message, match, expression);
-    } catch (std::regex_error& error) {
-        std::cout << "Your C++ compiler doesn't properly support regex. Please upgrade to GCC 4.9, Clang or newer.\n";
-        return 1;
-    }
+    std::regex expression = std::regex(matcher, std::regex_constants::ECMAScript | std::regex_constants::icase);
+    std::regex_search(message, match, expression);
 
     if (match.empty()) {
         return 1;
